91f3c9bf797e22e5c2f92d04be9f5a978fb89d27
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2022-10-12 23:14:32 +0200

    Ulepszony kalkulator

--- greetings/views.py
+++ greetings/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def greeting(request):
-    return HttpResponse("Hello Lord in my World")
+    return HttpResponse("Hello Lord in my World!! :) ")
 
 def personal_greeting(request, a):
-    return HttpResponse(f"Hello {a.capitalize()} in my World")
+    return HttpResponse(f"Hello {a.capitalize()} in my World!!! :) ")
