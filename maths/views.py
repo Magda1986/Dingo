cb21e371a14fa8ccead15addc2d4dc0378642256
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2022-10-13 17:53:28 +0200

    Kalkulator z obrazkiem

--- maths/views.py
+++ maths/views.py
@@ -1,58 +1,46 @@
-
-# Create your views here.
-
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import loader
+
 
 def math(request):
-   return HttpResponse("Tu będzie matma")
+    return HttpResponse("Tu będzie matma")
+
 
 def add(request, a, b):
-	a, b = int(a), int(b)
-	wynik = a+b
-	c = {"a":a, "b":b, "operacja": "+" ,"wynik":wynik}
-	return render(
-    	    request=request,
-    	    template_name="maths/main.html",
-    	    context=c
-	)
+    a, b = int(a), int(b)
+    wynik = a + b
+    t = loader.get_template("maths/operation.html")
+    c = {"a": a, "b": b, "operacja": "+", "wynik": wynik, "title": "dodawanie"}
+    return HttpResponse(t.render(c))
+
 
 def sub(request, a, b):
     a, b = int(a), int(b)
     wynik = a - b
-    c = {"a":a, "b":b, "operacja": "-" ,"wynik":wynik}
-    return render(
-    	    request=request,
-    	    template_name="maths/main.html",
-    	    context=c
-    )
+    t = loader.get_template("maths/operation.html")
+    c = {"a": a, "b": b, "operacja": "-", "wynik": wynik, "title": "odejmowanie"}
+    return HttpResponse(t.render(c))
 
 
 def mul(request, a, b):
-   a, b = int(a), int(b)
-   wynik = a * b
-   c = {"a": a, "b": b, "operacja": "*", "wynik": wynik}
-   return render(
-      request=request,
-      template_name="maths/main.html",
-      context=c
-   )
+    a, b = int(a), int(b)
+    wynik = a * b
+    t = loader.get_template("maths/operation.html")
+    c = {"a": a, "b": b, "operacja": "*", "wynik": wynik, "title": "mnożenie"}
+    return HttpResponse(t.render(c))
 
 
 def div(request, a, b):
-   a, b = int(a), int(b)
-   wynik = a / b
-   c = {"a": a, "b": b, "operacja": "/", "wynik": wynik}
-   if b == 0:
-       return HttpResponse("Nie dziel przez 0")
-   return render(
-      request=request,
-      template_name="maths/main.html",
-      context=c
-   )
+    a, b = int(a), int(b)
+    if b == 0:
+        return HttpResponse("Nie dziel przez 0")
+    wynik = a / b
+    t = loader.get_template("maths/operation.html")
+    c = {"a": a, "b": b, "operacja": "/", "wynik": wynik, "title": "dzielenie"}
+    return HttpResponse(t.render(c))
+
+
 
 #def div(request, a, b):
  #  a, b = int(a), int(b)
